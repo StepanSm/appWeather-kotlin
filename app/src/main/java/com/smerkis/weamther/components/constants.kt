b821acd28bd51878afb23d5f0ecf3653efba2198
Stepan Smerkis <stepis251@gmail.com>
2020-11-11 04:52:09 +0500

    local in master

---
+++ app/src/main/java/com/smerkis/weamther/components/constants.kt
@@ -0,0 +1,6 @@
+package com.smerkis.weamther.components
+
+import com.smerkis.weamther.BuildConfig
+
+const val API_KEY = BuildConfig.API_KEY
+const val WEATHER_URL = "http://api.openweathermap.org/data/2.5/"
